65582779b35fd7eacdd8bc17fa4d125e5a40a34e
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2025-03-28 00:02:49 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import Header from "../components/Header";
 import ProductList from "../components/ProductList";
 import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100">
+      <Header />
       <ProductList />
       <Footer />
     </div>
